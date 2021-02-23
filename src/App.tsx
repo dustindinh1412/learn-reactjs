47bfbc9417d3358abe0f9b00b06f26b0506098ee
KienDH <dustindinh.work@gmail.com>
2021-02-23 10:51:55 +0700

    Update name

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dinh Huu Kien
+          Dinh Huu Kien v2
         </p>
         <a
           className="App-link"
